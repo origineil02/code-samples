2601557d4adedbf0c0b22dbe98ecee1a67849d46
neilrayborn <neilrayborn@192.168.1.144>
2015-03-07 22:15:19 -0600

    Code Samples: Respoke : new project structure

---
+++ test/specs/models.client.spec.js
@@ -0,0 +1,17 @@
+
+console.log(typeof describe )
+
+'use strict';
+describe('The Client Model', function () {
+
+    describe('when we connect to a client', function () {
+        it('should make a connection', function (done) {
+            App.models.client('darth-vader', function (client) {
+                assert.equal(client.endpointId, 'darth-vader');
+                done();
+            });
+        });
+    });
+
+});
+console.log('End client test')
